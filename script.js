f358ba5306d1e6a7f174818fbc0660273834fb09
Sidonie Zollikofer <sidonie.zollikofer@studentfr.ch>
2025-12-15 14:40:20 +0100

    new changes

---
+++ script.js
@@ -0,0 +1,25 @@
+const links = [
+  "https://www.totallyuselesswebsites.com/",
+  "https://www.iamaturtle.org/",
+  "https://www.window-swap.com/",
+  "http://www.doughnutkitten.com/",
+  "https://stoplookingatmyscreen.com/",
+  "https://www.dramabutton.com/",
+  "https://www.emojitracker.com/",
+  "https://paveldogreat.github.io/WebGL-Fluid-Simulation/",
+  "https://more.playables.net/",
+  "https://www.culinaryschools.org/kids-games/pancake-pileup/",
+  "https://patatap.com/",
+  "https://anythingtranslate.com/translators/posh-english-translator/",
+  "https://anythingtranslate.com/translators/jealous-language-translator/",
+  "https://spiralbetty.com/",
+  "https://spinthewheel.app/good-or-evil-2xlI",
+  "https://theuselessweb.com/",
+  "http://www.staggeringbeauty.com/",
+];
+
+document.getElementById("boredBtn").addEventListener("click", () => {
+  const randomIndex = Math.floor(Math.random() * links.length);
+  const randomUrl = links[randomIndex];
+  window.location.href = randomUrl;
+});
